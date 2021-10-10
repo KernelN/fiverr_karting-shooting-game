da3a38e95aaf340f5f8bb8b9c8ed04b1f586c520
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-10-09 21:35:13 -0300

    Add Moving Enemy Kart

---
+++ car_game_fiver/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    internal enum MachineState { SEARCH, FOLLOW }
+
+    [SerializeField] Transform player;
+    internal MachineState state;
+    NavMeshAgent navMesh;
+
+    #region Unity Events
+    internal void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+    internal void Update()
+    {
+        RunStateMachine();
+    }
+    #endregion
+
+    virtual internal void GetPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    virtual internal void FollowPlayer()
+    {
+        navMesh.destination = player.position;
+    }
+    void RunStateMachine()
+    {
+        switch (state)
+        {
+            case MachineState.SEARCH:
+                GetPlayer();
+                if (player)
+                {
+                    state = MachineState.FOLLOW;
+                }
+                break;
+            case MachineState.FOLLOW:
+                if (!player)
+                {
+                    state = MachineState.SEARCH;
+                    return;
+                }
+                FollowPlayer();
+                break;
+            default:
+                break;
+        }
+    }
+}
