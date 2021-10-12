9d8dff8ac382beee80a854c52f15d29fb5d67435
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-10-12 13:35:58 -0300

    Reset counter when playing again

--- car_game_fiver/Assets/Scripts/GameManager.cs
+++ car_game_fiver/Assets/Scripts/GameManager.cs
@@ -4,9 +4,27 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
 {
     public bool playerWon;
     public int score;
+    string racingSceneName;
 
+    //Unity Actions
+    private void Start()
+    {
+        SceneManager.activeSceneChanged += OnSceneChange;
+    }
+
+    //Methods
     public void EndGame()
     {
+        racingSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(3);
+    }
+
+    //Actions
+    void OnSceneChange(Scene oldScene, Scene newScene)
+    {
+        if (newScene.name == "racingSceneName")
+        {
+            score = 0;
+        }
     }
 }
